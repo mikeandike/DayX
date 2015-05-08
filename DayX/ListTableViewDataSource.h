e11297ca6fcce7d92c918b588d569e472ec2253b
Michael Sacks <michaelsacks@gmail.com>
2015-05-08 13:48:13 -0600

    Labeled Cells Without Touch

---
+++ DayX/ListTableViewDataSource.h
@@ -0,0 +1,14 @@
+//
+//  ListTableViewDataSource.h
+//  DayX
+//
+//  Created by Michael Sacks on 5/8/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+@interface ListTableViewDataSource : NSObject <UITableViewDataSource>
+
+@end
---
+++ DayX/ListTableViewDataSource.m
@@ -0,0 +1,30 @@
+//
+//  ListTableViewDataSource.m
+//  DayX
+//
+//  Created by Michael Sacks on 5/8/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ListTableViewDataSource.h"
+
+static NSString *cellID = @"entryCell";
+
+@implementation ListTableViewDataSource
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    
+    cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Entry %ld" , indexPath.row + 1];
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 5;
+}
+
+
+@end
